36ac37812df9dabc230e0376603f4a359b6fbeba
Fathima Palot <99003564+palotfathima@users.noreply.github.com>
2026-09-25 20:44:56 +0530

    Create maxx.py

---
+++ maxx.py
@@ -0,0 +1,22 @@
+def max_negative(l1):
+  i = 0
+  count = 0
+
+  while i < len(l1):
+    if type(l1[i]) != int:
+      raise ValueError
+    else:
+      i += 1
+
+  i = 0
+  l2 = []
+
+  while i < len(l1):
+    if l1[i] < 0:
+      l2.append(l1[i])
+    i += 1
+
+  if len(l2) == 0:
+    raise ValueError
+
+  return max(l2)
