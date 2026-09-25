8c0b5d623c52dd2797c45e5c855e2fc65021950c
Fathima Palot <99003564+palotfathima@users.noreply.github.com>
2026-09-25 21:51:01 +0530

    Delete haadi.py

--- haadi.py
+++
@@ -1,9 +0,0 @@
-def first_negative_value_index(numbers):
-    i = 0
-
-    while i < len(numbers):
-        if numbers[i] < 0:
-            return [numbers[i], i]
-        i += 1
-
-    raise ValueError
