27cc7153489ccf537c7a732c0c7b7cf298bf8303
Fathima Palot <99003564+palotfathima@users.noreply.github.com>
2026-09-25 18:00:50 +0530

    Update haadi.py

--- haadi.py
+++ haadi.py
@@ -1,8 +1,9 @@
-def first_even_index(numbers):
+def first_negative_value_index(numbers):
     i = 0
-    
+
     while i < len(numbers):
-        if numbers[i] % 2 == 0:
-            return i
+        if numbers[i] < 0:
+            return [numbers[i], i]
         i += 1
-    
+
+    raise ValueError
