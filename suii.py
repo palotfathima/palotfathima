818d3879152f8d7c2b2732fa47d0becca5d335c1
Fathima Palot <99003564+palotfathima@users.noreply.github.com>
2026-09-25 20:39:06 +0530

    Update suii.py

--- suii.py
+++ suii.py
@@ -2,9 +2,8 @@ def min_positive(l1):
   i=0
   count=0
   while i<len(l1):
-    if type(i[l1]) != int:
+    if type(l1[i]) != int:
       raise ValueError
-      break
     else:
       i+=1
   i=0
@@ -13,7 +12,7 @@ def min_positive(l1):
     if l1[i]>0:
       l2.append(l1[i])
     i+=1
-  if i==0:
+  if len(l2)==0:
     raise ValueError
   return min(l2)   
     
